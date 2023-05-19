4563aaec08a5ced163d29605b1584d777179afee
Roberto Ramirez <robert.624@hotmail.com>
2023-05-19 17:21:06 -0500

    done and undoned check

--- src/components/TodoComponent/Todo.tsx
+++ src/components/TodoComponent/Todo.tsx
@@ -5,8 +5,10 @@ const Todo = ({text, done}:ToDo) => {
   return (
     <div className='todo-wrapper'>
         <div className="done-name">
+            {/* Only show check mark when todo is done */}
+            {done ? <img className='check' src="/icon-check.svg" alt="" />: ''}      
             <input checked={done} type="checkbox" name="done" id="done" />
-            <span className='todo-text'>{text}</span>
+            <span className={`todo-text ${done ? 'done' : ''}`}>{text}</span>
         </div>
         <img src="/icon-cross.svg" alt="" />
     </div>
